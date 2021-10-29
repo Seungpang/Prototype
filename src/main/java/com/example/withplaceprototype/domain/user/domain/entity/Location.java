4674ba1c10b8a1a7f75a265adca1b6b232156dc4
Kim Seunglae <obey1342@gmail.com>
2021-10-29 19:39:19 +0900

    feat: User엔티티 추가

---
+++ src/main/java/com/example/withplaceprototype/domain/user/domain/entity/Location.java
@@ -0,0 +1,19 @@
+package com.example.withplaceprototype.domain.user.domain.entity;
+
+import javax.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Location {
+
+    private Double longitude; //경도
+
+    private Double latitued; //위도
+}
