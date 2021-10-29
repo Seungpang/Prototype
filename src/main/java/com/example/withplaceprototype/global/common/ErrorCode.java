3efff784b76c7e84c75a980f503486306deb0770
Kim Seunglae <obey1342@gmail.com>
2021-10-29 19:40:11 +0900

    feat: Error관련 class 추가

---
+++ src/main/java/com/example/withplaceprototype/global/common/ErrorCode.java
@@ -0,0 +1,28 @@
+package com.example.withplaceprototype.global.common;
+
+public enum ErrorCode {
+
+    EMAIL_DUPLICATION(400, "U001", "Email is Duplication");
+
+    private final String code;
+    private final String message;
+    private int status;
+
+    ErrorCode(final int status, final String code, final String message) {
+        this.status = status;
+        this.message = message;
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
---
+++ src/main/java/com/example/withplaceprototype/global/common/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.example.withplaceprototype.global.common;
+
+import com.example.withplaceprototype.domain.user.exception.UserDuplicatedEmailException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+}
